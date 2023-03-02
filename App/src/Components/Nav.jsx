981bafb229e7f16b89b23d3749abbe206d4f4f99
Sebastian Marquez <66173614+Setimarz108@users.noreply.github.com>
2023-03-02 17:55:34 +0100

    responsive Navigation

--- App/src/Components/Nav.jsx
+++ App/src/Components/Nav.jsx
@@ -1,57 +1,59 @@
-import React, { useState } from "react";
-import { useMediaQuery, HStack, Flex, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, IconButton, Icon } from "@chakra-ui/react";
-import { FiMenu } from "react-icons/fi";
+import React from "react";
+import { useMediaQuery, Flex, HStack, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, DrawerFooter, Button, useDisclosure } from "@chakra-ui/react";
+
+import { Bars3Icon } from '@heroicons/react/24/solid'
 
 function Nav(props) {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
   const [isMobile] = useMediaQuery("(max-width: 767px)");
 
-  const handleDrawerOpen = () => {
-    setIsDrawerOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setIsDrawerOpen(false);
-  };
-
   return (
+
     <>
-      {isMobile ? (
-        <Flex justify="flex-end" p={2}>
-          <IconButton
-            icon={<Icon as={FiMenu} />}
-            variant="ghost"
-            onClick={handleDrawerOpen}
-            aria-label="Open navigation menu"
-          />
-        </Flex>
-      ) : (
+      {isMobile? (
+        <>
+<Bars3Icon ref={btnRef} colorScheme='teal' onClick={onOpen} width={"60px"}>
+</Bars3Icon>
+<Drawer
+isOpen={isOpen}
+placement='right'
+onClose={onClose}
+finalFocusRef={btnRef}
+>
+<DrawerOverlay />
+<DrawerContent>
+  <DrawerCloseButton />
+  <DrawerHeader>Create your account</DrawerHeader>
+
+  <DrawerBody>
+    {props.children}
+  </DrawerBody>
+
+  <DrawerFooter>
+    <Button variant='outline' mr={3} onClick={onClose}>
+      Cancel
+    </Button>
+    <Button colorScheme='blue'>Save</Button>
+  </DrawerFooter>
+</DrawerContent>
+</Drawer></>
+
+
+      ) : ( 
+
         <nav>
-          <Flex alignItems="center" gap="2">
-            <HStack direction="row" spacing={100} w={"100%"}>
-              {props.children}
-            </HStack>
-          </Flex>
-        </nav>
+        <Flex alignItems="center" gap="2">
+          <HStack direction="row" spacing={100} w={"100%"}>
+            {props.children}
+          </HStack>
+        </Flex>
+      </nav>
       )}
-      <Drawer isOpen={isDrawerOpen} onClose={handleDrawerClose} placement="left">
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader>Navigation</DrawerHeader>
-          <DrawerBody>
-            <Nav onClose={handleDrawerClose}>
-              <Flex alignItems="center" gap="2">
-                <HStack direction="row" spacing={100} w={"100%"}>
-                  {props.children}
-                </HStack>
-              </Flex>
-            </Nav>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
+     
+     
     </>
-  );
+  )
 }
 
 export default Nav;
