ef9e1dad7689b53bd7387cd929cb002fc01cc99b
Sebastian Marquez <66173614+Setimarz108@users.noreply.github.com>
2023-03-15 16:08:42 +0100

    ValidationSchema with Yup

--- App/src/Components/Reservation.jsx
+++ App/src/Components/Reservation.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Formik, Field, Form } from "formik";
+import * as Yup from "yup";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import {
   FormControl,
   FormLabel,
@@ -8,7 +9,19 @@ import {
   Button,
   Flex,
   Spacer,
+  Box,
 } from "@chakra-ui/react";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "Name must be at least 2 characters")
+    .required("Name is required"),
+  numOfGuests: Yup.number()
+    .min(1, "Must have at least 1 guest")
+    .required("Number of guests is required"),
+  date: Yup.date().required("Date is required"),
+  comments: Yup.string().min(3, "Comments cannot exceed 1000 characters"),
+});
 
 const ReservationForm = () => {
   return (
@@ -19,102 +32,92 @@ const ReservationForm = () => {
         date: "",
         comments: "",
       }}
-      validate={(values) => {
-        const errors = {};
-        if (!values.name) {
-          errors.name = "Required";
-        }
-        if (!values.numOfGuests) {
-          errors.numOfGuests = "Required";
-        }
-        if (!values.date) {
-          errors.date = "Required";
-        }
-        return errors;
-      }}
+      validationSchema={validationSchema}
       onSubmit={(values, actions) => {
         console.log(values);
         actions.setSubmitting(false);
       }}
     >
       {({ isSubmitting }) => (
-        <Form>
-          <Field name="name">
-            {({ field, form }) => (
-              <FormControl
-                isInvalid={form.errors.name && form.touched.name}
-                mb="4"
-              >
-                <FormLabel htmlFor="name">Name</FormLabel>
-                <Input {...field} id="name" placeholder="Name" />
-              </FormControl>
-            )}
-          </Field>
+        <Form className="Form">
+          <h1>Make your reservation!</h1>
+          <Flex direction="column">
+            <Box mb="4">
+              <Field name="name">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={form.errors.name && form.touched.name}
+                  >
+                    <FormLabel htmlFor="name">Name</FormLabel>
+                    <Input {...field} id="name" placeholder="Name"
+                    isInvalid
+                    errorBorderColor='red.300' />
+                    <ErrorMessage name="name" />
+                  </FormControl>
+                )}
+              </Field>
+            </Box>
 
-          <Field name="numOfGuests">
-            {({ field, form }) => (
-              <FormControl
-                isInvalid={
-                  form.errors.numOfGuests && form.touched.numOfGuests
-                }
-                mb="4"
-              >
-                <FormLabel htmlFor="numOfGuests">Number of Guests</FormLabel>
-                <Input
-                  {...field}
-                  id="numOfGuests"
-                  placeholder="Number of Guests"
-                  type="number"
-                  min="1"
-                />
-              </FormControl>
-            )}
-          </Field>
+            <Box mb="4">
+              <Field name="numOfGuests">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={
+                      form.errors.numOfGuests && form.touched.numOfGuests
+                    }
+                  >
+                    <FormLabel htmlFor="numOfGuests">Number of Guests</FormLabel>
+                    <Input
+                      {...field}
+                      id="numOfGuests"
+                      placeholder="Number of Guests"
+                      type="number"
+                      min="1"
+                    />
+                    <ErrorMessage name="numOfGuests" />
+                  </FormControl>
+                )}
+              </Field>
+            </Box>
 
-          <Field name="date">
-            {({ field, form }) => (
-              <FormControl
-                isInvalid={form.errors.date && form.touched.date}
-                mb="4"
-              >
-                <FormLabel htmlFor="date">Date</FormLabel>
-                <Input {...field} id="date" placeholder="Date" type="date" />
-              </FormControl>
-            )}
-          </Field>
+            <Box mb="4">
+              <Field name="date">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={form.errors.date && form.touched.date}
+                  >
+                    <FormLabel htmlFor="date">Date</FormLabel>
+                    <Input {...field} id="date" placeholder="Date" type="date" />
+                    <ErrorMessage name="date" />
+                  </FormControl>
+                )}
+              </Field>
+            </Box>
 
-          <Field name="comments">
-            {({ field, form }) => (
-              <FormControl
-                isInvalid={form.errors.comments && form.touched.comments}
-                mb="4"
-              >
-                <FormLabel htmlFor="comments">Comments</FormLabel>
-                <Textarea
+            <Box mb="4">
+              <Field name="comments">
+                {({ field, form }) => (
+                  <FormControl isInvalid={form.errors.comments && form.touched.comments}
+                  >
+                  <FormLabel htmlFor="comments">Comments</FormLabel>
+                  <Textarea
                   {...field}
                   id="comments"
                   placeholder="Comments"
-                  resize="none"
-                />
-              </FormControl>
-            )}
-          </Field>
-
-          <Flex>
-            <Spacer />
-            <Button
-              mt={4}
-              colorScheme="teal"
-              isLoading={isSubmitting}
-              type="submit"
-            >
-              Submit
-            </Button>
-          </Flex>
-        </Form>
-      )}
-    </Formik>
-  );
+                  />
+                  <ErrorMessage name="comments" />
+                  </FormControl>
+                  )}
+                  </Field>
+                  </Box>
+                  <Button colorScheme="blue" type="submit" isLoading={isSubmitting}>
+          Submit
+        </Button>
+      </Flex>
+    </Form>
+  )}
+</Formik>
+);
 };
 
 export default ReservationForm;
